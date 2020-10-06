07c8a31db7b3a26e268206d1316f1b1aa9b6f86e
baymax44 <smartbay2021@outlook.com>
2020-10-06 11:39:52 -0400

    add layout component

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Footer from '../comps/Footer'
 import NavBar from '../comps/Navbar'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1>Homepage</h1>
-      <Footer />
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
     </div>
   )
 }
