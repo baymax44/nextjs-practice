8f6b1a2f95f0c6df44fd4fdfd9b0262eb2f8fe0f
baymax44 <smartbay2021@outlook.com>
2020-10-12 00:51:06 -0400

    add image & metadata

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,17 @@
+import Head from 'next/head'
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is testing paragraph</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>This is testing paragraph</p>
+      </div>
+    </>
   );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Footer from '../comps/Footer'
-import NavBar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>This is homepage</p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>This is homepage</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
