4939b46d3e5cb3be969498e9be46624f313ec5f5
baymax44 <smartbay2021@outlook.com>
2020-10-13 00:51:39 -0400

    add fetch api sample

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,16 +1,26 @@
-import Head from 'next/head'
+import styles from '../../styles/Ninjas.module.css'
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
   return (
-    <>
-      <Head>
-        <title>Ninja List | Ninjas</title>
-        <meta name="keywords" content="ninjas" />
-      </Head>
-      <div>
-        <h1>Ninjas</h1>
-      </div>
-    </>
+    <div>
+      <h1>Ninjas</h1>
+      {ninjas.map(ninja => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
+    </div>
   );
 }
  
