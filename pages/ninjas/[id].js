54011e80520aebc8a76c78a5e0eb15780102b429
baymax44 <smartbay2021@outlook.com>
2020-10-15 00:28:03 -0400

    dynamic routing II

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,16 @@
+export const getStaticPath = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  const paths = data.map(ninja => {
+    return {
+      params: { id: ninja.id.toString() }
+    }
+  })
+
+  return { paths, fallback: false }
+}
+
 const Details = () => {
   return (
     <div>
