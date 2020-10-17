814d7bfedf94c495766ede262a884821a8e7c07b
baymax44 <smartbay2021@outlook.com>
2020-10-16 22:06:03 -0400

    get static value

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,4 +1,4 @@
-export const getStaticPath = async () => {
+export const getStaticPaths = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
 
@@ -11,10 +11,23 @@ export const getStaticPath = async () => {
   return { paths, fallback: false }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data }
+  }
+}
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ ninja.name }</h1>
+      <p>{ ninja.email }</p>
+      <p>{ ninja.website }</p>
+      <p>{ ninja.address.city }</p> 
     </div>
   );
 }
